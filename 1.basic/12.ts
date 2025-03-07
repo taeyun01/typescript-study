318a6d9c52bd23afbb010d2fefa3a54e21f60b1e
taeyun <lwe12f675@gmail.com>
2025-03-07 23:54:17 +0900

    feat: 타입좁히기(타입가드) 설명 추가

--- 1.basic/12.ts
+++ 1.basic/12.ts
@@ -1,8 +1,16 @@
+//* 타입 좁히기 (타입 가드)
+
+// a는 숫자 또는 문자열을 받을 수 있음
 function numOrStr(a: number | string) {
+  // a.toFixed(1); // TS는 모든 가능성을 다 고려하기 때문에 a가 number면 toFixed를 쓸 수 있는데, string일 가능성도 있기 때문에 경고를 띄어주는 것
+  // (a as number).toFixed(1); // as로 타입스크립트를 안심시켰더라도 자바스크립트 단에서 에러가남
+  // 예를 들어 함수 밖에서 numOrStr('123'); 호출하면 문자열에는 toFixed()를 쓸 수가 없음.
+  // unknown 일 때 빼고는 as는 안쓰는게 좋음, 아니면 남이 만들어진 타입이 틀렸을 때는 어쩔 수 없이 as를 씀
+  //* 이런식으로 타입 가드를 사용해서 타입 좁히기를 할 수 있음
   if (typeof a === "string") {
-    a.split(",");
+    a.split(","); // 문자열인것을 보장
   } else {
-    a.toFixed(1);
+    a.toFixed(1); // 숫자인것을 보장
   }
 }
 
